706993ded2e0da933b43e1bd5684a308a630b2be
Bandar-Zuhair <my.github.websites@gmail.com>
2026-07-15 19:16:44 +0700

    Update inv-company-setup.js

--- inv-company-setup.js
+++ inv-company-setup.js
@@ -1190,7 +1190,7 @@ function processInvoiceData(data) {
 
         // Determine the currency
         let currency = "SAR"; // Default currency
-        if (agencyUpper.includes("AL EZZ") || agencyUpper.includes("ALEZZ") || agencyUpper.includes("AL FAKHAMAH") || agencyUpper.includes("ALFAKHAMAH") || agencyUpper.includes("ALAM ALRAYA") || agencyUpper.includes("ALAM AL RAYA") || agencyUpper.includes("ATTAR")) {
+        if (agencyUpper.includes("AL EZZ") || agencyUpper.includes("ALEZZ") || /* agencyUpper.includes("AL FAKHAMAH") || agencyUpper.includes("ALFAKHAMAH") ||  */agencyUpper.includes("ALAM ALRAYA") || agencyUpper.includes("ALAM AL RAYA") || agencyUpper.includes("ATTAR")) {
             currency = "USD";
         } else if (guestByUpper.includes("RAYAN") || guestByUpper.includes("TURKI") || guestByUpper.includes("TURKEY") || guestByUpper.includes("TARIQ") || guestByUpper.includes("SECRET") || guestByUpper.includes("TURKY")) {
             currency = "IDR";
